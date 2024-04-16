8e65a7de54334550f651d96aede5df73c8ea2e8c
Vann-Dev <stevanvincent05@gmail.com>
2024-04-16 10:50:49 +0700

    feat: change workflow and add new field

--- src/routes/guild/post.ts
+++ src/routes/guild/post.ts
@@ -1,5 +1,4 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { SETTING_TYPE } from "../../typings/index.js";
 import prisma from "../../lib/prisma.js";
 
 export async function post(request: FastifyRequest, reply: FastifyReply) {
@@ -10,7 +9,14 @@ export async function post(request: FastifyRequest, reply: FastifyReply) {
         return reply.badGateway("No body provided");
     }
 
-    if (!Object.keys(body).find(key => SETTING_TYPE.find(type => type == key))) return reply.badGateway(`Not supported type. Supported type are ${SETTING_TYPE.join(", ")}`);
+    const bodyKeys = Object.keys(body);
+    const guildKeys = Object.keys(prisma.guild.fields);
+
+    const validKeys = bodyKeys.filter(key => guildKeys.includes(key));
+
+    if (validKeys.length !== bodyKeys.length) {
+        return reply.badGateway(`Invalid keys provided: ${bodyKeys.filter(key => !guildKeys.includes(key)).join(", ")}`);
+    }
 
     const updateSettings: any = {}
 
