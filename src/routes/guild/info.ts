7afbdedf573b738e5332037c2a09dde5ed59a8c0
Vann-Dev <stevanvincent05@gmail.com>
2024-09-22 22:29:47 +0700

    feat: using discordjs

--- src/routes/guild/info.ts
+++ src/routes/guild/info.ts
@@ -1,19 +1,11 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { discordClient } from "../../app.js";
+
 
 export async function info(request: FastifyRequest, reply: FastifyReply) {
     const params = request.params as { id: string };
 
-    const response = await fetch(`${process.env.DISCORD_API_BASE_URL}/guilds/${params.id}`, {
-        headers: {
-            "Authorization": `Bot ${process.env.DISCORD_TOKEN}`
-        }
-    })
-
-    if (!response.ok) {
-        return reply.status(404).send({ message: 'Guild not found' });
-    }
-
-    const data = await response.json()
+    const data = discordClient.guilds.cache.get(params.id)
 
     return reply.send({ data: data });
 }
