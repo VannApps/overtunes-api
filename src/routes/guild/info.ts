7dabcb65e043ce27fffd58ed9e8fe6978d927dec
Vann-Dev <stevanvincent05@gmail.com>
2024-09-22 23:39:34 +0700

    fix: build

--- src/routes/guild/info.ts
+++ src/routes/guild/info.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
-export async function info(request: FastifyRequest, reply: FastifyReply) {
-    const params = request.params as { id: string };
+export async function info(_request: FastifyRequest, reply: FastifyReply) {
+    // const params = request.params as { id: string };
 
     // const response = await fetch(`${process.env.DISCORD_API_BASE_URL}/guilds/${params.id}`, {
     //     headers: {
